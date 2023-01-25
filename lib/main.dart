3a05cafbe0c1e5129346a98f36ce1de021572993
raymolide <raymolide@gmail.com>
2023-01-25 16:25:06 +0200

    Add output audio

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@
 import 'dart:convert';
 import 'package:chat_bot/constantes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:http/http.dart' as http;
 import 'ChatMessage.dart';
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 void main() {
   runApp(MaterialApp(
@@ -22,16 +25,144 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
+enum TtsState { playing, stopped, paused, continued }
+
 class _MyAppState extends State<MyApp> {
   late bool isLoading;
   TextEditingController _textController = new TextEditingController();
   List<ChatMessage> messages = [];
   final _scrollController = ScrollController();
 
+  late FlutterTts flutterTts;
+  String? _newVoiceText;
+  TtsState ttsState = TtsState.stopped;
+  get isPlaying => ttsState == TtsState.playing;
+  get isStopped => ttsState == TtsState.stopped;
+  get isPaused => ttsState == TtsState.paused;
+  get isContinued => ttsState == TtsState.continued;
+  bool get isAndroid => !kIsWeb && Platform.isAndroid;
+  double volume = 0.5;
+  double pitch = 1.0;
+  double rate = 0.5;
+
   @override
   void initState() {
     isLoading = false;
     super.initState();
+    initTts();
+  }
+
+  initTts() {
+    flutterTts = FlutterTts();
+
+    _setAwaitOptions();
+
+    if (isAndroid) {
+      _getDefaultEngine();
+      _getDefaultVoice();
+    }
+
+    flutterTts.setStartHandler(() {
+      setState(() {
+        print("Playing");
+        ttsState = TtsState.playing;
+      });
+    });
+
+    if (isAndroid) {
+      flutterTts.setInitHandler(() {
+        setState(() {
+          print("TTS Initialized");
+        });
+      });
+    }
+
+    flutterTts.setCompletionHandler(() {
+      setState(() {
+        print("Complete");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setCancelHandler(() {
+      setState(() {
+        print("Cancel");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setPauseHandler(() {
+      setState(() {
+        print("Paused");
+        ttsState = TtsState.paused;
+      });
+    });
+
+    flutterTts.setContinueHandler(() {
+      setState(() {
+        print("Continued");
+        ttsState = TtsState.continued;
+      });
+    });
+
+    flutterTts.setErrorHandler((msg) {
+      setState(() {
+        print("error: $msg");
+        ttsState = TtsState.stopped;
+      });
+    });
+  }
+
+  Future _speak() async {
+    await flutterTts.setVolume(volume);
+    await flutterTts.setSpeechRate(rate);
+    await flutterTts.setPitch(pitch);
+
+    if (_newVoiceText != null) {
+      if (_newVoiceText!.isNotEmpty) {
+        await flutterTts.speak(_newVoiceText!);
+      }
+    }
+  }
+
+  Future _stop() async {
+    var result = await flutterTts.stop();
+    if (result == 1) setState(() => ttsState = TtsState.stopped);
+  }
+
+  Future _pause() async {
+    var result = await flutterTts.pause();
+    if (result == 1) setState(() => ttsState = TtsState.paused);
+  }
+
+  Future _setAwaitOptions() async {
+    await flutterTts.awaitSpeakCompletion(true);
+  }
+
+  Future _getDefaultEngine() async {
+    var engine = await flutterTts.getDefaultEngine;
+    if (engine != null) {
+      print(engine);
+    }
+  }
+
+  Future _getDefaultVoice() async {
+    var voice = await flutterTts.getDefaultVoice;
+    if (voice != null) {
+      print(voice);
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    flutterTts.stop();
+  }
+
+  void _onChange(String text) {
+    setState(() {
+      _newVoiceText = text;
+    });
   }
 
   Future<String> generateResponse(String prompt) async {
@@ -52,9 +183,15 @@ class _MyAppState extends State<MyApp> {
           'presence_penalty': 0.0
         }));
 
-    Map<String, dynamic> newResponse = jsonDecode(response.body);
-    print(response.body);
-    return newResponse['choices'][0]['text'];
+    if (response.statusCode == 201 || response.statusCode == 200) {
+      Map<String, dynamic> newResponse = jsonDecode(response.body);
+      print(response.body);
+      _onChange(newResponse['choices'][0]['text']);
+      _speak();
+      return newResponse['choices'][0]['text'];
+    }
+
+    return 'Sorry, podemos conversar mais tarde? Agora encontro-me indisponivel.';
   }
 
   @override
@@ -160,6 +297,7 @@ class _MyAppState extends State<MyApp> {
         itemCount: messages.length,
         itemBuilder: ((context, index) {
           var message = messages[index];
+          _newVoiceText = message.text;
           return ChatMessageWidget(
             text: message.text,
             chatMessageType: message.chatMessageType,
